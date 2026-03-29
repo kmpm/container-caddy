2b926b1e20d9884800f8cd381907adb575232455
Peter Magnusson <git@kmpm.se>
2026-03-29 08:09:55 +0000

    fix: add some output to entrypoint

--- ctx/root/usr/bin/entrypoint.sh
+++ ctx/root/usr/bin/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 
 CADDY_CONFIG=${CADDY_CONFIG:-/etc/caddy/Caddyfile}
@@ -9,9 +10,20 @@ echo CADDY_CONFIG=$CADDY_CONFIG
 echo CADDY_ADAPTER=$CADDY_ADAPTER
 echo PWD=$(pwd)
 
+# if first arg looks like a flag, assume we want to run caddy server
+if [ "${1:0:1}" = '-' ]; then
+    set -- caddy "$@"
+fi
 
 if [ "$1" = "caddy" ]; then
-    exec caddy run --watch --config $CADDY_CONFIG --adapter $CADDY_ADAPTER
+    echo "Running caddy..:"
+    # if no flags are provided, run caddy with our default flags
+    if [ "$#" -eq 1 ]; then
+        echo "No additional flags provided, running caddy with default flags"
+        set -- caddy run --watch --config $CADDY_CONFIG --adapter $CADDY_ADAPTER
+    fi
+
+    exec "$@"
 fi
 
 exec "$@"
