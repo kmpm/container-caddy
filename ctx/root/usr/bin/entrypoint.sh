c02b0500497daa76e4d064ec2a98b12022af9bba
Peter Magnusson <git@kmpm.se>
2026-03-29 09:33:35 +0000

    fix: manage existing gid

--- ctx/root/usr/bin/entrypoint.sh
+++ ctx/root/usr/bin/entrypoint.sh
@@ -8,6 +8,7 @@ CADDY_CONFIG=${CADDY_CONFIG:-/etc/caddy/Caddyfile}
 CADDY_ENVFILE=${CADDY_ENVFILE:-/etc/caddy/env}
 CADDY_ADAPTER=${CADDY_ADAPTER:-caddyfile}
 CADDY_USER=${CADDY_USER:-caddy}
+CADDY_GROUP=${CADDY_GROUP:-caddy}
 CADDY_FMT=${CADDY_FMT:-False}
 CADDY_GID=${CADDY_GID:-1000}
 CADDY_UID=${CADDY_UID:-1000}
@@ -20,6 +21,7 @@ echo CADDY_CONFIG=$CADDY_CONFIG
 echo CADDY_ENVFILE=$CADDY_ENVFILE
 echo CADDY_ADAPTER=$CADDY_ADAPTER
 echo CADDY_USER=$CADDY_USER
+echo CADDY_GROUP=$CADDY_GROUP
 echo CADDY_FMT=$CADDY_FMT
 echo CADDY_GID=$CADDY_GID
 echo CADDY_UID=$CADDY_UID
@@ -32,19 +34,29 @@ function set_permissions() {
         return
     fi
 
+    # get group of given CADDY_GID, if it exists
+    if getent group $CADDY_GID > /dev/null 2>&1; then
+        EXISTING_GROUP=$(getent group $CADDY_GID | cut -d: -f1)
+        echo "GID $CADDY_GID already exists for group $EXISTING_GROUP, using existing group"
+        CADDY_GROUP=$EXISTING_GROUP
+    else
+        echo "GID $CADDY_GID does not exist, creating group $CADDY_GROUP with GID $CADDY_GID"
+        addgroup -S -g $CADDY_GID $CADDY_GROUP
+    fi
+
     echo "Creating user $CADDY_USER with UID $CADDY_UID and GID $CADDY_GID"
-    addgroup -S -g $CADDY_GID $CADDY_USER && adduser -h /var/lib/$CADDY_USER -S -u $CADDY_UID $CADDY_USER -G $CADDY_USER
+    adduser -h /var/lib/$CADDY_USER -S -G $CADDY_GROUP -u $CADDY_UID $CADDY_USER
 
     echo "Setting permissions for $CADDY_USER on /var/lib/$CADDY_USER"
-    chown -R $CADDY_USER:$CADDY_USER /var/lib/$CADDY_USER
+    chown -R $CADDY_USER:$CADDY_GROUP /var/lib/$CADDY_USER
 
     if [ -n "$XDG_DATA_HOME" ]; then
         echo "Setting permissions for $CADDY_USER on XDG_DATA_HOME: $XDG_DATA_HOME"
-        chown -R $CADDY_USER:$CADDY_USER $XDG_DATA_HOME
+        chown -R $CADDY_USER:$CADDY_GROUP $XDG_DATA_HOME
     fi
     if [ -n "$XDG_CONFIG_HOME" ]; then
         echo "Setting permissions for $CADDY_USER on XDG_CONFIG_HOME: $XDG_CONFIG_HOME"
-        chown -R $CADDY_USER:$CADDY_USER $XDG_CONFIG_HOME
+        chown -R $CADDY_USER:$CADDY_GROUP $XDG_CONFIG_HOME
     fi
 
     echo "You have to make sure that the CADDY_USER has permissions to read the Caddyfile and any other files it needs to access"
